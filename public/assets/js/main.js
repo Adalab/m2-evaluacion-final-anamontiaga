285cb7fee39e1b2b1bd67cbfa3ba5cc475406bcd
anamontiaga <amontiaga@gmail.com>
2019-08-09 10:51:30 +0200

    add class to favorites with js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,6 +5,29 @@
 const btn = document.querySelector(".page__search--button");
 const listFilms = document.querySelector(".page__results");
 
+// array para guardar los favoritos
+const favFilms = [];
+
+function handleFav(event) {
+  const currentFilm = event.currentTarget;
+  // le añado una clase para que se convierta en favoritos al hacer click
+  currentFilm.classList.toggle("film--is--favorite");
+
+  console.log(currentFilm);
+}
+
+function activateFavs() {
+  const films = document.querySelectorAll(".page__results--list");
+  const filmsTitles = document.querySelectorAll(".page__results--name");
+  // para aplicarle las propiedades cuando clicko, primero las busco
+  for (const film of films) {
+    film.addEventListener("click", handleFav);
+  }
+  for (const title of filmsTitles) {
+    title.parentElement.addEventListener("click", handleFav);
+  }
+}
+
 // buscamos la película llamando a la API
 const getFilm = function() {
   let inputFilm = document.querySelector(".page__search--film");
@@ -18,6 +41,8 @@ const getFilmInfo = function(ev) {
   fetch(getFilm())
     .then(response => response.json())
     .then(data => {
+      listFilms.innerHTML = ""; // te lo pinta en blanco y no me repite las búsquedas y refresca
+
       for (let i = 0; i < data.length; i++) {
         let imgSrc;
         // // declaramos variable vacía con el src
@@ -30,11 +55,40 @@ const getFilmInfo = function(ev) {
 
         listFilms.innerHTML += `<li class="page__results--list"><img src="${imgSrc}" alt="${data[i].show.name}" class="page__results--image"><br/><h3 class="page__results--name">${data[i].show.name}</h3></li>`;
       }
+
+      activateFavs();
     });
 };
 
 btn.addEventListener("click", getFilmInfo);
 
 // cuando click en el resultado me añade una clase en la que el título es del color del fondo y viceversa
+
+// const resultFilmName = document.querySelectorAll(".page__results--name");
+
+// function handleFav(event) {
+//   const currentFilm = event.currentTarget;
+//   console.log(currentFilm);
+// }
+
+// function activateFavs() {
+//   const resultFilm = document.querySelectorAll(".page__results--list");
+//   for (const item of resultFilm) {
+//     item.addEventListener("click", handleFav);
+//   }
+// }
+
+// activateFavs();
+
+// function selectFilm() {
+//   for (const item of resultFilm) {
+//     resultFilm.style = "background-color: #a00957;";
+// for (const item of resultFilmName) {
+//   resultFilmName.style = "color: pink;";
+// }
+//   }
+// }
+
+// resultFilm.addEventListener("click", selectFilm);
 
 //# sourceMappingURL=main.js.map
